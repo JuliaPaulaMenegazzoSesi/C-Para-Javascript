10932523aef8dfdb5e1e8a43315ef57fa1907312
JuliaPaulaMenegazzoSesi <159256141+JuliaPaulaMenegazzoSesi@users.noreply.github.com>
2024-03-22 21:21:40 -0300

    Add files via upload

--- Exercício 4/script.js
+++ Exercício 4/script.js
@@ -6,53 +6,17 @@ function desconto(){
     if (tempo >= 5){
         if(valor > 5000){
             valor = valor - valor * 0.2
-            resultado = `Desconto: 20%<br>Valor a ser pago: ${valor}`
+            resultado = `Desconto: 20%<br>Valor a ser pago: R$${valor.toFixed(2)}`
         }
         else{
             valor = valor - valor * 0.1
-            resultado = `Desconto: 10<br>Valor a ser pago: ${valor}`
+            resultado = `Desconto: 10<br>Valor a ser pago: R$${valor.toFixed(2)}`
         }
     }
     else{
-        resultado = `Sem desconto<br>Valor a ser pago: ${valor}`
+        resultado = `Sem desconto<br>Valor a ser pago: R$${valor.toFixed(2)}`
     }
 
     document.getElementById("resultado").innerHTML = resultado
 }
 
-// #include<stdio.h>
-// #include<stdlib.h>
-// #include<locale.h>
-// int main(){
-// 	setlocale(LC_ALL,"Portuguese");
-	
-// 	int time;
-// 	float value;
-	
-// 	printf("INFORME O TEMPO DE FIDELIDADE:");
-// 	fflush(stdin);
-// 	scanf("%d",&time);
-// 	printf("INFORME O VALOR GASTO NA COMPRA :");
-// 	fflush(stdin);
-// 	scanf("%f",&value);
-	
-// 	if(time >= 5){
-// 		if(value > 5000){
-// 			printf("20%%.\n");
-// 		}
-// 		else{
-// 			if(value > 1000){
-// 			printf("10%%.\n");
-// 			}
-// 			else{
-// 				printf("SEM DESCONTO");
-// 			}
-// 		}	
-// 	}
-// 	else{
-// 		printf("SEM DESCONTO");
-// 	}
-	
-// 	system("pause");
-// 	return 0;
-// }
--- Exercício 5/script.js
+++ Exercício 5/script.js
@@ -19,31 +19,3 @@ function calcular(){
 
     document.getElementById("resultado").innerHTML = resultado
 }
-// #define hora 10.00
-// #define horaextra 15.00
-	
-// 	printf("Informe o numero de horas normais trabalhadas no ano:\n");
-// 	fflush(stdin);
-// 	scanf("%f",&horas_normais);
-	
-// 	printf("Informe o numero de horas extras trabalhadas no ano:\n");
-// 	fflush(stdin);
-// 	scanf("%f",&horas_extras);
-	
-// 	horasnorm_resul = horas_normais * hora;
-// 	horasext_resul = horas_extras * horaextra;
-// 	resul = horasnorm_resul + horasext_resul;
-	
-// 	poup = resul * 0.08;
-	
-// 	printf("______________________________________________________\n");
-// 	printf("Valor de horas normais: %.2f reais\n\n",horasnorm_resul);
-// 	printf("Valor de horas extras: %.2f reais\n\n",horasext_resul);
-// 	printf("Total de ganho no ano = %.2f reais\n\n", resul);
-// 	printf("Valor a guardar na poupança = %.2f reais\n\n", poup);
-// 	printf("______________________________________________________\n");
-	
-	
-// 	system("pause");
-// 	return 0;
-// }
